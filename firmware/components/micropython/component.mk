39c3dd650e4bcba15b0746195a3c94c16a9aafce
Renze Nicolai <renze@rnplus.nl>
2019-07-28 14:15:58 +0200

    Add 8bck (PocketSprite) audio mixer

--- firmware/components/micropython/component.mk
+++ firmware/components/micropython/component.mk
@@ -109,6 +109,7 @@ MP_EXTRA_INC += -I$(PROJECT_PATH)/components/driver_framebuffer/include
 MP_EXTRA_INC += -I$(PROJECT_PATH)/components/driver_led_neopixel/include
 MP_EXTRA_INC += -I$(PROJECT_PATH)/components/driver_display_eink/include
 MP_EXTRA_INC += -I$(PROJECT_PATH)/components/driver_io_disobey_samd/include
+MP_EXTRA_INC += -I$(PROJECT_PATH)/components/sndmixer
 MP_EXTRA_INC += -I$(PROJECT_PATH)/components/png
 
 
@@ -193,6 +194,7 @@ SRC_C =  $(addprefix esp32/,\
 	modespnow.c \
 	moddisobeysamd.c \
 	modframebuffer.c \
+	modsndmixer.c \
 	)
 
 ifdef CONFIG_DRIVER_I2C_ENABLE
